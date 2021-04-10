89078200d3215b77eacfaf14a6666ea4cf984ac6
Rokoucha <git@rokoucha.net>
2021-04-10 21:31:21 +0900

    refactor: 🌳🤝 tree shaking

--- webpack.config.js
+++ webpack.config.js
@@ -5,24 +5,17 @@ const { join } = require('path')
  * @type{ import('webpack').Configuration }
  */
 module.exports = {
-  output: {
-    filename: `worker.js`,
-    path: join(__dirname, 'dist'),
-  },
   mode: 'production',
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
-    plugins: [],
-  },
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
         loader: 'ts-loader',
-        options: {
-          transpileOnly: true,
-        },
+        options: { transpileOnly: true },
+        test: /\.tsx?$/,
       },
     ],
   },
+  optimization: { usedExports: true },
+  output: { filename: 'worker.js', path: join(__dirname, 'dist') },
+  resolve: { extensions: ['.ts', '.tsx', '.js'], plugins: [] },
 }
